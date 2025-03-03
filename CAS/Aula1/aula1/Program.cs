59b31e3bfe9c3b2d91cd51f314b6091478b198e6
Ana Julia <anajufg127@gmail.com>
2025-03-02 21:43:35 -0300

    Update Program.cs

--- CAS/Aula1/aula1/Program.cs
+++ CAS/Aula1/aula1/Program.cs
@@ -1,10 +1,17 @@
-﻿using CAS;
+using CAS;
 
 Expressao a = 10;
-Expressao b = 0;
+Simbolo b = new Simbolo("b");
+Simbolo c = new Simbolo("c");
 Expressao numComplexo = new NumeroComplexo(10, -5);
 
 Expressao soma = a + b;
 Expressao divisao = a / b;
 
-Console.WriteLine(divisao.Simplificar());
+Console.WriteLine(soma.Simplificar());
+
+soma = soma.Substituir(b, 5);
+
+Expressao bt = b.Substituir(c, 3);
+
+Console.WriteLine(bt.ToString());
